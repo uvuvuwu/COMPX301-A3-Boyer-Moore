6bbd35464b8e72156c3f87f12c210c153e9e45dc
uvuvuwu <2003danielsu@gmail.com>
2024-04-30 12:46:04 +1200

    Make sure user inputs 2 arguments for MakeBMTable

--- src/main/java/MakeBMTable.java
+++ src/main/java/MakeBMTable.java
@@ -1,3 +1,20 @@
+/*
+ * MakeBMTable creates a skip table on a given string
+ * Inputs are
+ * 1. String to search
+ * 2. Name of the file to store the skip table to
+ */
 public class MakeBMTable {
-    
+    public static void main(String [] args) {
+        // If there isn't 2 arguments, tell user to put in 2 arguments or else >:(
+        if(args.length != 2){
+            System.out.println("Write: java MakeBMTable stringToSearch skipTableFileName >:(");
+            System.exit(0);
+        }
+
+        String substring = args[0];
+        String fileName = args[1];
+
+        
+    }
 }

4a8d4209e3a55661bdf7ca4472eeb63debe126aa
Alma Walmsley <almawalmsley@gmail.com>
2024-05-01 22:57:02 +1200

    Refactor MakeBMTable to use BMTable

--- src/main/java/MakeBMTable.java
+++ src/main/java/MakeBMTable.java
@@ -2,7 +2,6 @@
 // Name: Alma Walmsley  |  ID: 1620155
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,7 +14,7 @@ import java.io.BufferedWriter;
  * 2. Name of the file to store the skip table to
  */
 public class MakeBMTable {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
         // If there isn't 2 arguments, tell user to put in 2 arguments or else >:(
         if(args.length != 2){
             System.out.println(">:( write: java MakeBMTable stringToSearch skipTableFileName");
@@ -23,78 +22,57 @@ public class MakeBMTable {
         }
 
         // Store arguments given by user
-        String stringToSearch = args[0];
-        String skipTableFileName = args[1];
+        String inputString = args[0];
+        String outputFilename = args[1];
 
-        // Build empty table
-        ArrayList<Character> uniqueLetters = findAllUniqueLetters(stringToSearch);
-        SkipTable skipTable = new SkipTable(stringToSearch, uniqueLetters);
-        skipTable.buildEmptyTable();
-
-        // Fill the empty table
-        skipTable.fillTable();
+        // Create a new BMTable object
+        BMTable table = new BMTable(inputString);
+        // Populate the BMTable object with the skip amounts
+        table.fill();
 
         // Output the table to a file, file name specified by the user
-        outputTable(skipTable, skipTableFileName);
+        outputTable(table, outputFilename);
     }
 
-    /*
-     * findAllUniqueLetters finds and returns a list of all the unique letters in a passed in string
-     */
-    public static ArrayList<Character> findAllUniqueLetters(String stringToSearch){
-        ArrayList<Character> uniqueLettersList = new ArrayList<>();
-        for (int i = 0; i < stringToSearch.length(); i++) {
-            char c = stringToSearch.charAt(i);
-            if (!uniqueLettersList.contains(c)) {
-                uniqueLettersList.add(c);
-            }
-        }
-        return uniqueLettersList;
-    }
 
     /*
      * outputTable writes the generated skipTable out to a file
      */
-    public static void outputTable(SkipTable skipTable, String skipTableFileName){
-        // Stores the strings to output to skip table
-        ArrayList<String> rows = new ArrayList<>();
-        String tempRow = "";
+    public static void outputTable(BMTable table, String filename) throws IOException {
+        File file = new File(filename);
+        file.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
 
         // Generate first row of skip table 
-        tempRow = "*";
-        for(int i = 0; i < skipTable.getNumColumns(); i++){
-            tempRow = tempRow + "," + skipTable.getStringToSearch().charAt(i);
+        String tempRow = "*";
+        for (int i = 0; i < table.getNumColumns(); i++){
+            tempRow += "," + table.getSearchString().charAt(i);
         }
-        rows.add(tempRow);
-        
+        writer.write(tempRow);
+        writer.newLine();
+
         // Generate rest of rows of skip table
-        int tableNumRows = skipTable.getNumRowsInTable();
-        for(int i = 0; i < tableNumRows; i++){
-            tempRow = Character.toString(skipTable.getSkipTable().get(i).getRowCharacter());
-            for(int j = 0; j < skipTable.getNumColumns(); j++){
-                tempRow = tempRow + "," + skipTable.getSkipTable().get(i).getRow()[j];
+        char[] tableChars = table.getRowCharacters();
+        BMTableRow tableRow;
+        for (int i = 0; i < tableChars.length; i++) {
+            tempRow = "" + tableChars[i];
+            if (i == tableChars.length - 1) {
+                // If it's the last row, set the row to the default row
+                tableRow = table.getDefaultRow();
+            } else {
+                // Otherwise, get the row from the table
+                tableRow = table.getRow(tableChars[i]);
             }
-            rows.add(tempRow);
-        }
-
-        // Generate a file and write the rows to the file
-        try {
-            File file = new File(skipTableFileName);
-            file.createNewFile();
-            BufferedWriter myWriter = new BufferedWriter(new FileWriter(skipTableFileName));
-
-            for(int i = 0; i < rows.size(); i++){
-                myWriter.write(rows.get(i));
-                // Go to new line except on the last row to write
-                if(i != rows.size() - 1){
-                    myWriter.newLine();
-                }
+            for (int j = 0; j < table.getNumColumns(); j++){
+                tempRow += "," + tableRow.getSkipAmount(j);
             }
-            myWriter.close();
-
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            writer.write(tempRow);
+            if (i != tableChars.length - 1) {
+                // Write a newline if it's not the last row
+                writer.newLine();
+            }
         }
+        // Close the writer
+        writer.close();
     }
 }
